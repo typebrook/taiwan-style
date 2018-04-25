1f5846ea07b4d92b0c0b0771cc53a3052961e4a4
Hsieh Chin Fan (Pham) <typebrook@gmail.com>
2018-04-26 00:35:49 +0800

    Update handle-tif.sh

--- handle-tif.sh
+++ handle-tif.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# download DEM file from MOI. I file is invalid, see https://data.gov.tw/dataset/35430
+# download DEM file from MOI. If file is invalid, see https://data.gov.tw/dataset/35430
 wget -O MOI-20mDEM.zip  http://data.moi.gov.tw/MoiOD/System/DownloadFile.aspx?DATA=89476AAA-53A8-46C6-8161-0E38B700B772
 
 # decompress the zip file
